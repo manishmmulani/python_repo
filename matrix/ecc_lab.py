12a571f625de80b1f9a4a9ce4310e8511592f39c
Manish Mulani <mulanimonty@gmail.com>
2013-08-12 03:59:13 +0530

    Completed Ecc lab.

--- matrix/ecc_lab.py
+++ matrix/ecc_lab.py
@@ -1,27 +1,40 @@
 from vec import Vec
 from mat import Mat
-from bitutil import noise
+from matutil import listlist2mat, mat2coldict,coldict2mat
+from bitutil import noise,str2bits,bits2mat
 from GF2 import one
+from vecutil import list2vec
 
 ## Task 1 part 1
 """ Create an instance of Mat representing the generator matrix G. You can use
 the procedure listlist2mat in the matutil module (be sure to import first).
 Since we are working over GF (2), you should use the value one from the
 GF2 module to represent 1"""
-G = None
+G = listlist2mat([[one,0,one,one],
+                  [one,one,0,one],
+                  [0,0,0,one],
+                  [one,one,one,0],
+                  [0,0,one,0],
+                  [0,one,0,0],
+                  [one,0,0,0]])
 
 ## Task 1 part 2
 # Please write your answer as a list. Use one from GF2 and 0 as the elements.
-encoding_1001 = None
+encoding_1001 = [0,0,one,one,0,0,one]
 
 
 ## Task 2
 # Express your answer as an instance of the Mat class.
-R = None
+R = listlist2mat([[0,0,0,  0,0,  0,one],
+                  [0,0,0,  0,0,  one,0],
+                  [0,0,0,  0,one,0,  0],
+                  [0,0,one,0,0,  0,  0]])
 
 ## Task 3
 # Create an instance of Mat representing the check matrix H.
-H = None
+H = listlist2mat([[0,0,0,one,one,one,one],
+                  [0,one,one,0,0,one,one],
+                  [one,0,one,0,one,0,one]])
 
 ## Task 4 part 1
 def find_error(e):
@@ -34,16 +47,22 @@ def find_error(e):
         >>> find_error(Vec({0,1,2}, {2:one}))
         Vec({0, 1, 2, 3, 4, 5, 6},{0: one})
         >>> find_error(Vec({0,1,2}, {1:one, 2:one}))
-        Vec({0, 1, 2, 3, 4, 5, 6},{2: one})    
+        Vec({0, 1, 2, 3, 4, 5, 6},{2: one})
     """
-    pass
+    hcoldict = mat2coldict(H)
+    index = None
+    for collabel, colvec in hcoldict.items():
+        if colvec == e:
+            index=collabel
+    resultdict = {} if index==None else {index:one}
+    return Vec({0, 1, 2, 3, 4, 5, 6}, resultdict)
 
 ## Task 4 part 2
 # Use the Vec class for your answers.
 non_codeword = Vec({0,1,2,3,4,5,6}, {0: one, 1:0, 2:one, 3:one, 4:0, 5:one, 6:one})
-error_vector = Vec(..., ...)
-code_word = Vec(..., ...)
-original = ... # R * code_word
+error_vector = Vec({0,1,2,3,4,5,6}, {6:one})
+code_word = Vec({0,1,2,3,4,5,6}, {0:one, 1:0, 2:one, 3:one, 4:0, 5:one})
+original = list2vec([0,one,0,one]) # R * code_word
 
 
 ## Task 5
@@ -56,20 +75,20 @@ def find_error_matrix(S):
         >>> find_error_matrix(S)
         Mat(({0, 1, 2, 3, 4, 5, 6}, {0, 1, 2, 3}), {(1, 2): 0, (3, 2): one, (0, 0): 0, (4, 3): one, (3, 0): 0, (6, 0): 0, (2, 1): 0, (6, 2): 0, (2, 3): 0, (5, 1): one, (4, 2): 0, (1, 0): 0, (0, 3): 0, (4, 0): 0, (0, 1): 0, (3, 3): 0, (4, 1): 0, (6, 1): 0, (3, 1): 0, (1, 1): 0, (6, 3): 0, (2, 0): 0, (5, 0): 0, (2, 2): 0, (1, 3): 0, (5, 3): 0, (5, 2): 0, (0, 2): 0})
     """
-    pass
+    return coldict2mat({collabel:find_error(colvec) for collabel, colvec in mat2coldict(S).items()})
 
 ## Task 6
 s = "I'm trying to free your mind, Neo. But I can only show you the door. You’re the one that has to walk through it."
-P = None
+P = bits2mat(str2bits(s))
 
 ## Task 7
-C = None
-bits_before = None
-bits_after = None
+C = G*P
+bits_before = 896
+bits_after = 1568
 
 
 ## Ungraded Task
-CTILDE = None
+CTILDE = C + noise(C, 0.02)
 
 ## Task 8
 def correct(A):
@@ -81,4 +100,4 @@ def correct(A):
         >>> correct(A)
         Mat(({0, 1, 2, 3, 4, 5, 6}, {1, 2, 3}), {(0, 1): 0, (1, 2): 0, (3, 2): 0, (1, 3): 0, (3, 3): 0, (5, 2): one, (6, 1): 0, (3, 1): 0, (2, 1): 0, (0, 2): one, (6, 3): one, (4, 2): 0, (6, 2): one, (2, 3): 0, (4, 3): 0, (2, 2): 0, (5, 1): 0, (0, 3): one, (4, 1): 0, (1, 1): 0, (5, 3): one})
     """
-    pass
+    return A + find_error_matrix(H*A)
--- matrix/test_mat_mul.py
+++ matrix/test_mat_mul.py
@@ -1,13 +1,41 @@
+from GF2 import one
 from mat import Mat
 from vec import Vec
+from vecutil import list2vec
 from matutil import *
 from hw3 import *
+from ecc_lab import G,encoding_1001, R, H, find_error,non_codeword,P,s,C,CTILDE
+from bitutil import *
 
-rowlabels = {0, 1}
-collabels = {0, 1}
-matdict = {(0,0):3, (0,1):4,
-           (1,0):2, (1,1):1}
-mat = Mat((rowlabels, collabels), matdict)
+print(G)
+print(list2vec(encoding_1001))
+print(R*list2vec(encoding_1001))
+print(R*G)
+print(H*G)
+print("0,1,2 -> 0:1" + str(find_error(Vec({0,1,2}, {2:one}))))
+
+#print(P)
+print("len(P) : " + str(len(P.D[0])) + "*" + str(len(P.D[1])))
+print("No. of bits before : " + str(len(str2bits(s))))
+E_P = P + noise(P, 0.02)
+print(bits2str(mat2bits(E_P)))
+print("No. of bits after : " + str(len(mat2bits(C))))
+print(bits2str(mat2bits(R*CTILDE)))
+#c_ = non_codeword
+#print("c_" + str(c_))
+#e = find_error(H*c_)
+#print("error vector e : " + str(e))
+#c = c_ + e
+#print("code word : " + str(c))
+#print("original : " + str(R*c))
+
+#print(H*c)
+
+#rowlabels = {0, 1}
+#collabels = {0, 1}
+#matdict = {(0,0):3, (0,1):4,
+#           (1,0):2, (1,1):1}
+#mat = Mat((rowlabels, collabels), matdict)
 #print(str(lin_comb_mat_vec_mult(mat,vec)))
 #print(mat)
 #print(dot_prod_mat_mat_mult(mat, mat))
@@ -18,10 +46,16 @@ mat = Mat((rowlabels, collabels), matdict)
 #8x + 4y = 4
 #5x = 4
 #x= 4/5
+#3,5,6,7 bits
+#0 0 1 0 0 0 0   0 1 1 1 1 0 0
+#0 0 0 0 1 0 0
+#0 0 0 0 0 1 0
+#0 0 0 0 0 0 1
+
 
 #y = -3/5
-print(solving_systems_m)
-AB = solving_systems_m*mat
-BA = mat*solving_systems_m
-print(AB)
-print(BA)
+#print(solving_systems_m)
+#AB = solving_systems_m*mat
+#BA = mat*solving_systems_m
+#print(AB)
+#print(BA)
