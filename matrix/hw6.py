95b9e1cbc447df93f3f1b41a6a8a4abe171a0a96
Manish Mulani <mulanimonty@gmail.com>
2013-08-25 20:30:53 +0530

    Completed hw6 stencil

--- matrix/hw6.py
+++ matrix/hw6.py
@@ -2,6 +2,7 @@
 # Please fill out this stencil and submit using the provided submission script.
 
 from matutil import *
+from vecutil import zero_vec,list2vec
 from GF2 import one
 
 
@@ -9,25 +10,25 @@ from GF2 import one
 ## Problem 1
 # Write each matrix as a list of row lists
 
-echelon_form_1 = [[   ...   ],
-                  [   ...   ],
-                  [   ...   ],
-                  [   ...   ],
-                  [   ...   ]]
+echelon_form_1 = [[   1,2,0,2,0   ],
+                  [   0,1,0,3,4   ],
+                  [   0,0,2,3,4   ],
+                  [   0,0,0,2,0   ],
+                  [   0,0,0,0,4   ]]
 
-echelon_form_2 = [[   ...   ],
-                  [   ...   ],
-                  [   ...   ],
-                  [   ...   ]]
+echelon_form_2 = [[   0,4,3,4,4   ],
+                  [   0,0,4,2,0   ],
+                  [   0,0,0,0,1   ],
+                  [   0,0,0,0,0   ]]
 
-echelon_form_3 = [[   ...   ],
-                  [   ...   ],
-                  [   ...   ]]
+echelon_form_3 = [[   1,0,0,1   ],
+                  [   0,0,0,1   ],
+                  [   0,0,0,0   ]]
 
-echelon_form_4 = [[   ...   ],
-                  [   ...   ],
-                  [   ...   ],
-                  [   ...   ]]
+echelon_form_4 = [[   1,0,0,0   ],
+                  [   0,1,0,0   ],
+                  [   0,0,0,0   ],
+                  [   0,0,0,0   ]]
 
 
 
@@ -45,16 +46,26 @@ def is_echelon(A):
         >>> is_echelon([[0,1,1],[0,1,0],[0,0,1]])
         False
     '''
-    pass
-
-
+    c=0
+    for r in range(len(A)):
+        for i in range(c):
+            if A[r][i] != 0:
+                return False
+        firstNonZeroFound=False
+        for i in range(c,len(A[r])):
+            if (A[r][i] != 0):
+                c=i+1
+                firstNonZeroFound=True
+                break
+        if not firstNonZeroFound:
+            c=len(A[r])
+    return True
 
 ## Problem 3
 # Give each answer as a list
-
-echelon_form_vec_a = ...
-echelon_form_vec_b = ...
-echelon_form_vec_c = ...
+echelon_form_vec_a = [1,0,3,0]
+echelon_form_vec_b = [-3,0,-2,3]
+echelon_form_vec_c = [-5,0,2,0,2]
 
 
 
@@ -62,8 +73,8 @@ echelon_form_vec_c = ...
 # If a solution exists, give it as a list vector.
 # If no solution exists, provide "None".
 
-solving_with_echelon_form_a = ...
-solving_with_echelon_form_b = ...
+solving_with_echelon_form_a = None
+solving_with_echelon_form_b = [21,0,2,0,0]
 
 
 
@@ -79,55 +90,66 @@ def echelon_solve(rowlist, label_list, b):
         - Vec x such that rowlist * x is b
     >>> D = {'A','B','C','D','E'}
     >>> U_rows = [Vec(D, {'A':one, 'E':one}), Vec(D, {'B':one, 'E':one}), Vec(D,{'C':one})] 
-    >>> b_list = [one,0,one]>>> cols = ['A', 'B', 'C', 'D', 'E']
+    >>> b_list = [one,0,one]
+	>>> cols = ['A', 'B', 'C', 'D', 'E']
     >>> echelon_solve(U_rows, cols, b_list)
     Vec({'B', 'C', 'A', 'D', 'E'},{'B': 0, 'C': one, 'A': one})
     '''
-    pass
-
-
+    sorted_ll = sorted(label_list)
+    solution = zero_vec(set(label_list))
+    for r in range(len(rowlist)-1,-1,-1):
+        indexOfFirstNonZero=0
+        firstNonZeroFound=False
+        for c in sorted_ll:
+            if rowlist[r][c] != 0:
+                firstNonZeroFound=True
+                indexOfFirstNonZero=c
+                break
+        if firstNonZeroFound:
+            solution[indexOfFirstNonZero] = b[r] - solution*rowlist[r] # if it's not GF(2) then divide rhs by coeff which is rowlist[r][c]
+    return solution
 
 ## Problem 6
-rowlist = [ ... ]    # Provide as a list of Vec instances
-label_list = [ ... ] # Provide as a list
-b = [ ... ]          # Provide as a list
-
-
+myDomainList=['A','B','C','D']
+myDomain=set(myDomainList)
+rowlist = [ Vec(myDomain, {'A':one,'B':one,'D':one}), Vec(myDomain, {'B':one}), Vec(myDomain, {'C':one}), Vec(myDomain,{'D':one}) ]    # Provide as a list of Vec instances
+label_list = myDomainList  # Provide as a list
+b = [ one,one,0,0 ]          # Provide as a list
 
 ## Problem 7
-null_space_rows_a = {...} # Put the row numbers of M from the PDF
+null_space_rows_a = {3,4} # Put the row numbers of M from the PDF
 
 
 
 ## Problem 8
-null_space_rows_b = {...}
+null_space_rows_b = {4}
 
 
 
 ## Problem 9
 # Write each vector as a list
-closest_vector_1 = [...]
-closest_vector_2 = [...]
-closest_vector_3 = [...]
+closest_vector_1 = [1.6, 3.2]
+closest_vector_2 = [0,1,0]
+closest_vector_3 = [3,2,1,-4]
 
 
 
 ## Problem 10
 # Write each vector as a list
 
-project_onto_1 = [...]
-projection_orthogonal_1 = [...]
+project_onto_1 = [2,0]
+projection_orthogonal_1 = [0,1]
 
-project_onto_2 = [...]
-projection_orthogonal_2 = [...]
+project_onto_2 = [-1.0/6, -1.0/3, 1.0/6]
+projection_orthogonal_2 = [7.0/6, 4.0/3, 23.0/6]
 
-project_onto_3 = [...]
-projection_orthogonal_3 = [...]
+project_onto_3 = [1,1,4]
+projection_orthogonal_3 = [0,0,0]
 
 
 
 ## Problem 11
-norm1 = ...
-norm2 = ...
-norm3 = ...
+norm1 = 3
+norm2 = 4
+norm3 = 1
 
